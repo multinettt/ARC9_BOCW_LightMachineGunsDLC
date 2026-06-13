5c9997c805eda73d9d662897e3b5a7cbad5180e1
multinett <72457258+multinettt@users.noreply.github.com>
2026-06-13 18:40:08 +0300

    SWEP.SubCategory

--- lua/weapons/arc9_bocw_mg82.lua
+++ lua/weapons/arc9_bocw_mg82.lua
@@ -29,7 +29,7 @@ SWEP.Base = "arc9_base"
 
 SWEP.Spawnable = true
 SWEP.Category = "ARC9 - Black Ops Cold War"
-SWEP.SubCategory = "4Light Machine Guns"
+SWEP.SubCategory = "3Light Machine Guns"
 SWEP.AdminOnly = false
 
 SWEP.PrintName = "MG 82"
